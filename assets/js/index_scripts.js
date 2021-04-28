0e86556a643cd03b73274412ec785f361cd8ed8a
Carlos Mosquera <camosqueraba@gmail.com>
2021-04-28 16:00:59 -0500

    agredos scripts de lectura de archivos

--- assets/js/index_scripts.js
+++ assets/js/index_scripts.js
@@ -5,4 +5,46 @@ function clickMenu() {
     else{
       document.getElementById("contenedor_opciones_menu").style.visibility = "visible";
     }
-  }
+}
+
+const slider = document.getElementById("#contenido-carousel");   
+
+function cargarSliders(){
+
+	fetch('assets/database/carousel.json')
+	.then(respuesta => respuesta.json())
+	.then(sliders => {
+		sliders.results.forEach(slider => {
+
+			const contenido_slider = document.createElement('div');
+			contenido_slider.classList='carousel-item';
+			/*
+			contenido_slider.innerHTML('<a class="scroll-link" ' + 'href="'+${contenido_slider.id} +'">'+
+										 '<img src="assets/img/slides/'+${usuario.id}+' " class="d-block w-100" alt="'+ ${usuario.id}+'">
+	       							   </a>'+
+	       								'<div class="carousel-caption">'+
+											'<a class="scroll-link" href="#anuncio2">'+
+												'<h3 class="carousel-titulo">Feliz Dia Profesionales de Enfermeria</h3>'+
+												'<div class="carousel-caption-description">'+
+													'<p>Feliz dia a todos los profesionales de enfermeria.</p>'+
+												'</div>'+
+											'</a>'+
+										'</div>');
+			*/
+			contenido_slider.innerHTML(`<a class="scroll-link"  href="${contenido_slider.id}">
+										 <img src="assets/img/slides/${usuario.id}" class="d-block w-100" alt="${contenido_slider.titulo}">
+	       							   </a>
+	       								<div class="carousel-caption">
+											<a class="scroll-link" href="${contenido_slider.id}">
+												<h3 class="carousel-titulo">${contenido_slider.titulo}</h3>
+												<div class="carousel-caption-description">
+													<p>${contenido_slider.descripcion}</p>
+												</div>
+											</a>
+										</div>`);
+			sliders.appendChild(contenido_slider);
+		});
+	})
+}
+
+cargarSliders();
